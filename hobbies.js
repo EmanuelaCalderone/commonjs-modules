7a3424799c3807a5edac7888b66463693c17f5f3
Emanuela Calderone <emanuelacalderone3006@gmail.com>
2025-01-31 15:51:37 +0100

    add hobbies

---
+++ hobbies.js
@@ -0,0 +1,7 @@
+function hobbies(hobbyOne, hobbyTwo, hobbyThree) {
+    return [hobbyOne, hobbyTwo, hobbyThree];
+}
+
+//console.log(hobbies("calcio", "pallavolo", "nuoto"));
+
+module.exports = hobbies;
